508bfd0bf56f18f61c1ec406c60951c770c8a90b
DanPo12 <147959151+DanPo12@users.noreply.github.com>
2025-04-06 20:05:35 -1000

    Add files via upload

--- connecting satelite.py
+++ connecting satelite.py
@@ -4,14 +4,21 @@ import random
 WIDTH=600
 HEIGHT=600
 TITLE="connect the satelite"
+list=[]
 
-s=Actor("stli")
-s.x=300
-s.y=300
+for i in range(8):
+    s=Actor("stli")
+    s.x=random.randint(0,600)
+    s.y=random.randint(0,600)
+    list.append(s)
 
 def draw():
     screen.blit("spb",(0,0))
-    s.draw()
+    number=1
+    for s in list:
+        s.draw()
+        screen.draw.text(str(number),(s.x,s.y+30),color="red")
+        number=number+1
 def update():
     pass
 
